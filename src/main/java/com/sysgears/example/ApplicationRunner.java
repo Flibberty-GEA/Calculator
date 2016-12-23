8a524db1d5abb0141333d80a6fd1107fa428b396
Yevgen Goliuk <yevgeniy.golyuk@gmail.com>
2016-12-23 11:33:09 +0200

    add log4j

--- src/main/java/com/sysgears/example/ApplicationRunner.java
+++ src/main/java/com/sysgears/example/ApplicationRunner.java
@@ -1,6 +1,8 @@
 package com.sysgears.example;
 
 import com.sysgears.example.controller.RequestController;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 /**
  * Main class of program
@@ -9,12 +11,16 @@ import com.sysgears.example.controller.RequestController;
  */
 public class ApplicationRunner {
 
+    public static final Logger log = LogManager.getLogger(ApplicationRunner.class);
+
     /**
      * Entrance point to program.
      *
      * @param args command line input arguments
      */
     public static void main(String[] args) {
+        log.info("Start program.");
+
         try{
             /* initialize main execution object and start the program */
             new RequestController().run();
